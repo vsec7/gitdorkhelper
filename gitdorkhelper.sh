ca4f6039587e5d3b0a4845530072613501920f9b
Viloid <vsec48@gmail.com>
2021-02-28 13:36:01 +0700

    Add output .html

--- gitdorkhelper.sh
+++ gitdorkhelper.sh
@@ -2,8 +2,12 @@
 # GitHub Dork Helper
 # Ve <3 Cath
 # Keyword list from https://github.com/obheda12/GitDorker/tree/master/Dorks
-# Usage : ./gitdorkhelper "target" all_dorks.txt
+# Usage : ./gitdorkhelper "target" all_dorks.txt [output.html *optional]
+# Example : ./gitdorkhelper "paypal" all_dorks.txt out.html
 
 while read i; do
     echo "[ $i ] https://github.com/search?q=%22$1%22+`echo $i | sed -e 's/ /+/g' | sed -e 's/\"/%22/g'`&type=Code"
+    if [ ! -z $3 ]; then
+         echo "<li><input type=\"checkbox\"> [<a href=\"https://github.com/search?q=%22$1%22+`echo $i | sed -e 's/ /+/g' | sed -e 's/\"/%22/g'`&type=Code\" target=\"_blank\"> $i </a>]</li>" >> $3
+    fi
 done < $2
